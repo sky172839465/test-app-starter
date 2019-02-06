f80bc2787c845894b2acf19fccb7fb8ca85d9699
Yusong Hsu <sky172839465@gmail.com>
2019-02-07 01:10:06 +0800

    Update for deploy

---
+++ ghpage-deploy.sh
@@ -0,0 +1,7 @@
+cd build
+git init
+git config user.name "Travis CI"
+git config user.email "yusong.demo@gmail.com"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1

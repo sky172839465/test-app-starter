7f6ca237bcd4b7ba1afb6841293a668e714894e6
Yusong Hsu <sky172839465@gmail.com>
2019-02-07 12:09:49 +0800

    Update for functional & visual test

---
+++ codecept/local.config.js
@@ -0,0 +1,18 @@
+exports.config = {
+  name: 'test-app',
+  tests: '../src/__e2e__/src/**/**.js',
+  output: '../report',
+  helpers: {
+    EyesHelper: { require: './helper/eyesHelper.js' },
+    WebDriverIO: {
+      url: 'http://localhost:3000',
+      browser: 'chrome',
+      waitForTimeout: 300000
+    }
+  },
+  include: {
+    I: './actor/steps_file.js'
+  },
+  bootstrap: false,
+  coloredLogs: true
+}
